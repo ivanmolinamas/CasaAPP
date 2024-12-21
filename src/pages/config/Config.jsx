c8077a91425149ac1411c06a15685d4821257961
Ivan Molina <ivanmolinamas@gmail.com>
2024-12-21 11:48:51 +0100

    ADD - se empieza pagina configuracion

--- src/pages/config/Config.jsx
+++ src/pages/config/Config.jsx
@@ -1,28 +1,65 @@
+import { getUsers } from "../../services/dbconnect";
 import classes from "./Config.module.css";
-
-
+import { useState } from "react";
+import ButtonRa from "../../components/button/ButtonRa";
 
 
 
 export default function Config() {
 
-  // Comprobamos token
-useEffect(() => {
-  getDevicesStateNow(); //Pedimos los datos al backend de los dispositivos
-  const token = localStorage.getItem('token');
-  if (!token) {
-    // Si no hay token, redirige al login
-    window.location.href = '/';
-  } else {
-    // Verifica el token con el servidor
-    verifyToken();
+  const [usuarios, setUsuarios] = useState([]); // Estado para almacenar los usuarios
+
+  async function mostrarUsuarios() {
+    try {
+      const usuarios = await getUsers();
+      console.log("Usuarios:", usuarios);
+      // Aquí podrías renderizar la lista en tu interfaz, por ejemplo:
+      usuarios.forEach((usuario) => {
+        console.log(`ID: ${usuario.id}, User: ${usuario.user}, Email: ${usuario.email}, Rol: ${usuario.rol}`);
+      });
+      if (usuarios.length > 0 ) {
+        setUsuarios(usuarios); // Actualizamos el estado con los usuarios
+      } else {
+        console.error("Error al obtener usuarios:", response.message);
+      }
+    } catch (error) {
+      console.error("Error al obtener usuarios:", error.message);
+    }
   }
-}, []);
 
 
   return (
     <div className={classes.container}>
-      <h1>Configuracion</h1>
+      <h1>Configuración</h1>
+      <ButtonRa onClick={mostrarUsuarios} >Mostrar Usuarios</ButtonRa>
+     {/* Renderizar la tabla solo si hay usuarios */}
+     <h2>Lista de Usuarios</h2>
+     <div className={classes.tableContainer}>
+     {usuarios.length > 0 ? (
+        <table className={classes.table}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Usuario</th>
+              <th>Email</th>
+              <th>Rol</th>
+            </tr>
+          </thead>
+          <tbody>
+            {usuarios.map((usuario) => (
+              <tr key={usuario.id}>
+                <td>{usuario.id}</td>
+                <td>{usuario.user}</td>
+                <td>{usuario.email}</td>
+                <td>{usuario.rol}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>No se han cargado usuarios.</p>
+      )}
+     </div>
     </div>
   );
 }
