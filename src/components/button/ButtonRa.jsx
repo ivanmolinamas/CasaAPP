468b389f6782a94b28db22e5267c5c1dfc3db252
Ivan Molina <ivanmolinamas@gmail.com>
2024-12-21 11:42:32 +0100

    FIX - se corrige boton para que lance eventos y se pase mejor el texto con children

--- src/components/button/ButtonRa.jsx
+++ src/components/button/ButtonRa.jsx
@@ -1,13 +1,13 @@
 import classes from "./ButtonRa.module.css";
 import { Button } from "@radix-ui/themes";
 
-export default function ButtonRa({ texto }) {
+export default function ButtonRa({ children , onClick}) {
 
     
 
     return (
-      <Button size="4" >
-        {texto}
+      <Button size="4" onClick={onClick}>
+        {children}
       </Button>
     )
 
